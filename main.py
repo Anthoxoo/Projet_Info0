903df295fe475c56bca9ea3daef81381f6135108
Antoine <briocheguenot@gmail.com>
2026-03-10 16:29:51 +0100

    fonction afficher terminée

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def creer_grille():
+def creer_grille() -> list[list]:
     plateau = []
 
     for i in range(8):
@@ -12,10 +12,12 @@ def creer_grille():
     ajouter_noirs_grille(plateau)
     ajouter_blancs_grille(plateau)
 
-    afficher_plateau(plateau)
+    afficher_plateau(plateau, "noirs")
+
+    return plateau
 
 
-def ajouter_noirs_grille(plateau):
+def ajouter_noirs_grille(plateau: list[list]):
     for j in range(1, 8, 2):
         plateau[0][j] = "n"  # 1ere rangée
         plateau[2][j] = "n"  # 3eme rangée
@@ -24,7 +26,7 @@ def ajouter_noirs_grille(plateau):
         plateau[1][j] = "n"  # 2eme rangée
 
 
-def ajouter_blancs_grille(plateau):
+def ajouter_blancs_grille(plateau: list[list]):
     for j in range(1, 8, 2):
         plateau[7][j] = "b"
         plateau[5][j] = "b"
@@ -33,26 +35,34 @@ def ajouter_blancs_grille(plateau):
         plateau[6][j] = "b"
 
 
-def est_dans_grille(plateau):
+def est_dans_grille(plateau: list[list]):
     pass
 
 
-def est_au_bon_format(plateau):
+def est_au_bon_format(plateau: list[list]):
     pass
 
 
-def afficher_plateau(plateau):
+# print(f"...") permet de mettre une variable dans un print et d'éviter de faire print("..." + variable + "...")
+def afficher_plateau(plateau: list[list], tour_de_jeu: str):
+    lettres = ["H", "G", "F", "E", "D", "C", "B", "A"]
+    # Permet d'afficher les lettres sur le coté, a chaque itération on pop le tableau.
+
     print("""
---------------------------------
- 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
---------------------------------""")
+--------------------------------------
+  x |  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+--------------------------------------""")
 
-    for i in plateau:
-        for j in i:
-            print(f" {j} |", end="")
+    for ligne in plateau:
+        print(f"| {lettres.pop()} | ", end="")
+
+        for colonne in ligne:
+            print(f" {colonne} |", end="")
+
         print("")
 
-    # TODO: mettre les lettres a coté et faire en sorte que l'on puisse savoir a quel tour c'est.
+    print("--------------------------------------")
+    print(f"C'est au tour des pions {tour_de_jeu} de jouer.")
 
 
 creer_grille()
